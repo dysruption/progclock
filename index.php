b49ef94f5a38c6e77b5f5882f72fa418adbec8cd
Mark Mossberg <mark.mossberg@gmail.com>
2013-10-08 18:03:38 -0400

    clean up

--- index.php
+++ index.php
@@ -3,47 +3,33 @@
     <link rel="stylesheet" type="text/css" href="style.css">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
     <script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
-<script type='text/javascript'src='main.js'></script>
+    <script type='text/javascript'src='main.js'></script>
 	<script type="text/javascript" src="script.js"></script>
-<script type='text/javascript'>
-//$(function (){
-
-	
-//});
-
-</script>
-
+    <script type='text/javascript'>
+    </script>
     <title>progclock.</title>
-    <h1>just enough time</h1>
 </head>
 <body>
-	
+    <h1>just enough time</h1>
 	<hr>
-	
 	<nav id="input">
 	</nav>
-
-	<form id="myform" NAME="myform" METHOD="GET" autocomplete="off">
+	<form id="myform" name="myform" method="GET" autocomplete="off">
 		<span class="bracket">[</span>
 		<input type="text" id="day" placeholder="day"> : <input type="text" id="hour" placeholder="hour"> : <input type="text" id="minute" placeholder="minute"> : <input type="text" id="second" placeholder="second">
 		<span class="bracket">]</span>
 		<br>
 		<input id="button" type="button" name="button" value="Submit">
 	</form>
-
 	<div id="timer">00:00:00:00</div>
 	<div id="progress">
 	  <div id="bar" style="background-color:#999999; width:2; height: 76;">&nbsp;</div>
 	</div>
 	<br>
-
 	<div id=activity></div>
 	<?php
 	//	include('phrase.php');
 	?>
-	
-
-<div id="foot"><a href="http://www.markmossberg.us/">Mark Mossberg</a> &amp; <a href="http://www.jameslarisch.com/">James Larisch</a></div>
-
+    <div id="foot"><a href="http://www.markmossberg.us/">Mark Mossberg</a> &amp; <a href="http://www.jameslarisch.com/">James Larisch</a></div>
 </body>
 </html>
